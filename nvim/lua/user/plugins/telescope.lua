ed77e8fd73964adaf0b5fa15b208f07e80f9cb50
Guillermo Cava <gcavanunez@gmail.com>
2024-06-11 22:57:21 -0500

    updated harpoon

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -131,7 +131,11 @@ return {
     local ui = require("harpoon.ui")
 
     vim.keymap.set("n", "<leader>a", mark.add_file)
-    vim.keymap.set("n", "<C-p>", ui.toggle_quick_menu)
+    vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end, { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end, { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end, { noremap = true, silent = true })
+    vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end, { noremap = true, silent = true })
     vim.keymap.set("n","<leader><Tab>", function() ui.nav_next() end)
     vim.keymap.set("n","<leader><S-Tab>", function() ui.nav_prev() end)
     -- vim.keymap.set("n","<leader>hc", function() mark.clear_all() end)
