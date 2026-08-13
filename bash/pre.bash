f9fbd270f3d1321bdff48ac51dc1700c3e7b3407
Guillermo <gcavanunez@gmail.com>
2026-08-13 01:46:12 -0500

    prefer distro blesh when available

--- bash/pre.bash
+++ bash/pre.bash
@@ -1,5 +1,9 @@
 # Load ble.sh before prompt and history integrations, then attach it in init.bash.
-BLESH_PATH="${XDG_DATA_HOME:-$HOME/.local/share}/blesh/ble.sh"
+if [[ -f /usr/share/blesh/ble.sh ]]; then
+  BLESH_PATH=/usr/share/blesh/ble.sh
+else
+  BLESH_PATH="${XDG_DATA_HOME:-$HOME/.local/share}/blesh/ble.sh"
+fi
 if [[ $- == *i* && -f "$BLESH_PATH" ]]; then
   source -- "$BLESH_PATH" --attach=none
 fi
